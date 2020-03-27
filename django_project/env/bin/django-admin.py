4974b00f23410f03cf57894a808a449004a1257e
JoshGarzagit config --global user.email jegarza98@gmail..com <jegarza98@gmail.com>
2020-03-27 15:50:47 -0500

    add login,logout,profile system

---
+++ django_project/env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/JoshGarza/Dev/Python-Dev/Django_Blog_Solution/06-User-Registration-Form/django_project/env/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
--- env/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/Users/JoshGarza/Dev/Python-Dev/django_blog/env/bin/python3
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
