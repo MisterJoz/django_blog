6278f42b89f9d5bf06559f3764d67efc8929d245
JoshGarzagit config --global user.email jegarza98@gmail..com <jegarza98@gmail.com>
2020-03-30 13:56:06 -0500

    Add user profile w/ signals

---
+++ django_project/venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/JoshGarza/Dev/Python-Dev/django_blog/django_project/venv/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
